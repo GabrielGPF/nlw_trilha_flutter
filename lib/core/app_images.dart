1e6636eaf87cf417a721feb0c7190dbf73641804
Gabriel Fardoski <43648010+GabrielGPF@users.noreply.github.com>
2021-04-21 17:56:51 -0300

    Assets added, unnecessary comments deleted, SplashPage created

---
+++ lib/core/app_images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  static String get hierarchy => "lib/assets/images/hierarchy.png";
+  static String get data => "lib/assets/images/data.png";
+  static String get laptop => "lib/assets/images/laptop.png";
+  static String get blocks => "lib/assets/images/blocks.png";
+  static String get check => "lib/assets/images/check.png";
+  static String get error => "lib/assets/images/error.png";
+  static String get trophy => "lib/assets/images/trophy.png";
+  static String get logo => "lib/assets/images/logo.png";
+}
---
+++ lib/core/app_widget.dart
@@ -0,0 +1,15 @@
+// import 'package:nlw_trilha_flutter/home/home_page.dart';
+// import 'package:nlw_trilha_flutter/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+import 'package:nlw_trilha_flutter/pages/splash_page.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "DevQuiz",
+      home: SplashPage(), //HomePage(),
+    );
+  }
+}
