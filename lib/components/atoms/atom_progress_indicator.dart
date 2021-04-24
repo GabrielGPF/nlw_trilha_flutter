aef4f640c5ed4d0e90b1784f499dde7e199ad184
Gabriel Fardoski <43648010+GabrielGPF@users.noreply.github.com>
2021-04-23 21:17:07 -0300

    ProgressIndicatorWidget created

---
+++ lib/components/atoms/atom_progress_indicator.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:nlw_trilha_flutter/core/app_colors.dart';
+
+class ProgressIndicatorWidget extends StatelessWidget {
+  final double value;
+
+  ProgressIndicatorWidget({
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearProgressIndicator(
+      value: 0.3,
+      backgroundColor: AppColors.chartSecondary,
+      valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+    );
+  }
+}
--- lib/components/molecules/molecule_quiz_card.dart
+++ lib/components/molecules/molecule_quiz_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nlw_trilha_flutter/components/atoms/atom_progress_indicator.dart';
 import 'package:nlw_trilha_flutter/core/app_colors.dart';
 import 'package:nlw_trilha_flutter/core/app_images.dart';
 import 'package:nlw_trilha_flutter/core/app_text_styles.dart';
@@ -43,12 +44,7 @@ class QuizCard extends StatelessWidget {
               SizedBox(width: 24),
               Expanded(
                 flex: 2,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor:
-                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-                ),
+                child: ProgressIndicatorWidget(),
               ),
             ],
           ),
